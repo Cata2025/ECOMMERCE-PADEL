ae7d42429b649dc795a27752ff838585fbaba654
Catalina <cblvallada@gmail.com>
2024-12-02 00:35:34 +0100

    Delete endpoint user

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -50,6 +50,41 @@ const userController = {
             res.status(500).send({ message: "There was a problem", error });
         
           }
+    },
+    async update(req,res){
+        const id = req.params.id
+        const newUserData = req.body; // Data to update (sent in the request body)
+
+        newUserData.password = await bcrypt.hash(newUserData.password, 10)
+        console.log("NEW USER DATA")
+        console.log(newUserData)
+
+        const user = await User.findByPk(id);
+        await user.update(newUserData);
+
+        res.status(200).json({
+            message: 'User updated successfully',
+            user,
+          });
+    },
+    async delete(req,res){
+        try {
+            const { id } = req.params; 
+        
+         
+            const user = await User.findByPk(id);
+            if (!user) {
+              return res.status(404).json({ message: 'User not found' });
+            }
+        
+          
+            await user.destroy();
+        
+            res.status(200).json({ message: 'User deleted successfully' });
+          } catch (error) {
+            console.error('Error deleting user:', error);
+            res.status(500).json({ message: 'Error deleting user', error });
+          }
     }
 }
 
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -6,6 +6,9 @@ const userRouter = express.Router()
 userRouter.get("/logout",authentication,userController.logout)
 userRouter.post("/login",userController.login)
 userRouter.post("/create", userController.create)
+userRouter.patch("/:id", userController.update)
+userRouter.delete("/:id", userController.delete)
+
 
 
 
